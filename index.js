d577df4fdd4f31f5f28ae1b7a0146c70799d8abe
Amani Hayes-Messinger <amani@Amanis-MBP-2.fios-router.home>
2020-09-23 19:07:04 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".js-typing", {
+    strings: ["Web Developer", "Project Manager", "Writer"],
+    showCursor: false,
+    typeSpeed: 60,
+    backSpeed: 60,
+    smartBackspace: false,
+    shuffle: false,
+    loop: true
+  });
+
+  
